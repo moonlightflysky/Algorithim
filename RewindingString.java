c93d7d4c556e43e39019a622cab0e66a04a541d6
Haoyang Li <moonlightflysky@hotmail.com>
2016-06-16 13:43:28 -0700

    Create RewindingString.java

---
+++ RewindingString.java
@@ -0,0 +1,56 @@
+package inter_string;
+
+public class RewindingString {
+	
+	public boolean isMultipileDuplicate(String s){
+		
+		if (s == null || s.length() <= 1){
+			return false;
+		}
+		
+		int pStart = 0, pEnd = 0;
+		int N = s.length();
+		int i = 0;
+		
+		while (i < N){
+			
+			//System.out.println(pStart);
+			
+			if (s.charAt(i) != s.charAt(pStart)){
+				if (pStart == 0){
+					pEnd = i;
+					i++;
+				}
+				else{
+					pEnd = i - 1;
+					pStart = 0;
+				}
+				
+			}
+			
+			else{
+				if (pStart == pEnd){
+					if( i != s.length() - 1){
+						pStart = 0;
+					}
+				}
+				else{
+					pStart++;
+				}
+				i++;
+			}
+			
+		}
+		
+		return pStart == pEnd && pEnd > 0;
+		
+	}
+	
+	public static void main(String[] argv){
+		
+		RewindingString test = new RewindingString();
+		System.out.println(test.isMultipileDuplicate("abcdabcdeabcdabcde"));
+		
+	}
+
+}
